93051f1f2abdebc71211f4d0c001a88736975ce1
92B5A9 <capellagao1127@gmail.com>
2026-08-25 22:53:19 +0800

    Update submit.js

--- functions/submit.js
+++ functions/submit.js
@@ -10,3 +10,12 @@ export async function onRequestPost({ request, env }) {
     headers: { "Content-Type": "application/json" }
   });
 }
+export async function onRequestGet({ env }) {
+  const result = await env.DB.prepare(
+    "SELECT * FROM posts ORDER BY id DESC"
+  ).all();
+
+  return new Response(JSON.stringify(result), {
+    headers: { "Content-Type": "application/json" }
+  });
+}
