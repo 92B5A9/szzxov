dd6c5f749b30214eda2d0bcea5d16f6b500c4586
92B5A9 <capellagao1127@gmail.com>
2026-08-25 23:10:06 +0800

    Update submit.js

--- functions/submit.js
+++ functions/submit.js
@@ -1,21 +1,12 @@
 export async function onRequestPost({ request, env }) {
   const data = await request.json();
-  const { title, author } = data;
+  const { title, author, issue } = data;
 
   await env.DB.prepare(
-    "INSERT INTO posts (title, author) VALUES (?, ?)"
-  ).bind(title, author).run();
+    "INSERT INTO posts (title, author, issue) VALUES (?, ?, ?)"
+  ).bind(title, author, issue).run();
 
   return new Response(JSON.stringify({ ok: true }), {
-    headers: { "Content-Type": "application/json" }
-  });
-}
-export async function onRequestGet({ env }) {
-  const result = await env.DB.prepare(
-    "SELECT * FROM posts ORDER BY id DESC"
-  ).all();
-
-  return new Response(JSON.stringify(result), {
     headers: { "Content-Type": "application/json" }
   });
 }
