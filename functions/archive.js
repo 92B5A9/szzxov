7648f73cf482266ec0c33ddc60359886287cfa41
92B5A9 <capellagao1127@gmail.com>
2026-08-26 15:20:26 +0800

    Delete functions directory

--- functions/archive.js
+++
@@ -1,9 +0,0 @@
-export async function onRequestGet({ env }) {
-  const result = await env.DB.prepare(
-    "SELECT * FROM posts ORDER BY id DESC"
-  ).all();
-
-  return new Response(JSON.stringify(result), {
-    headers: { "Content-Type": "application/json" }
-  });
-}
